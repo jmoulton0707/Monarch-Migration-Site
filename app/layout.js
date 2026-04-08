9b7a6f2a1716cd698f273018d98666c8488a0b2a
jmoulton0707 <jmoulton7@gmail.com>
2026-04-07 21:41:24 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,7 @@ export const metadata = {
   description: "AWS-native data migration as a service",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
